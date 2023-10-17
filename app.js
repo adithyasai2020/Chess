cdef7dcb5accfb3551201dc39c288fdc89a8f69b
adithyasai2020 <77226888+adithyasai2020@users.noreply.github.com>
2023-10-18 00:41:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const gameboard = document.getElementById("gameboard");
+const playerDisplay = document.getElementById("player");
+const infoDisplay = document.getElementById("info-display");
+const width = 8
+
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
+    "", "", "", "", "", "", "", "", 
+    "", "", "", "", "", "", "", "", 
+    "", "", "", "", "", "", "", "", 
+    "", "", "", "", "", "", "", "", 
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
+];
+
+function createBoard(){
+    startPieces.forEach((startPiece, i)=>{
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.classList.add('beige');
+        if(Math.floor((63-i)/8)%2 == 0){
+            if(i%2 == 0){
+                square.classList.add('beige');
+            }
+            else{
+                square.classList.add('brown');
+            }
+        }
+        else{
+            if(i%2){
+                square.classList.add('beige');
+            }
+            else{
+                square.classList.add('brown');
+            }
+
+        }
+        square.innerHTML = startPiece;
+        square.firstChild?.setAttribute('draggable', true)
+        square.setAttribute('square-id', i);
+
+        
+        if(i<=15){
+            square.firstChild.firstChild.classList.add('black');
+        }
+        else if(i>=64-16){
+            square.firstChild.firstChild.classList.add('white');
+
+        }
+        
+        gameboard.appendChild(square);
+    
+    })
+}
+
+function loadChess(){
+    
+    createBoard();
+
+
+}
